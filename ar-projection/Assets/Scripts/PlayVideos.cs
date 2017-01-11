78355cc33de90a1bcaaa162abd70d54096e20f1a
Lincoln Green <lingojs@me.com>
2017-01-10 22:24:54 -0800

    add basic AR scene in Scenes/Houndstooth

---
+++ ar-projection/Assets/Scripts/PlayVideos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideos : MonoBehaviour {
+	[SerializeField] private MovieTexture[] videos;
+
+	void Awake()
+	{
+		foreach (var v in videos)
+			v.loop = true;
+	}
+
+	void OnEnable()
+	{
+		foreach (var v in videos) {
+			v.Stop ();
+			v.Play ();
+		}
+	}
+}
